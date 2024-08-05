6d36e0021b386ac95e88254b0805297ebbce1528
Valerian5788 <valerianvdcpro@gmail.com>
2024-08-05 15:48:07 +0200

    Add project files.

---
+++ Controllers/TweetsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using TwitterAnalyzer.Models;
+
+namespace TwitterAnalyzer.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TweetsController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public TweetsController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpPost("classify")]
+        public async Task<IActionResult> Classify([FromBody] TweetRequest tweetRequest)
+        {
+            var json = JsonConvert.SerializeObject(tweetRequest);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("http://localhost:5001/classify", content);
+            var classificationResult = await response.Content.ReadAsStringAsync();
+
+            return Ok(classificationResult);
+        }
+    }
+}
---
+++ Models/TweetRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TwitterAnalyzer.Models
+{
+    public class TweetRequest
+    {
+        public List<string> Tweets { get; set; }
+    }
+}
