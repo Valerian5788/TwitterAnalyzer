07e85e8045ce48d26550b6de8e70177aab7d0209
Valerian5788 <valerianvdcpro@gmail.com>
2024-08-06 11:51:25 +0200

    ensuring json ok

--- Controllers/TweetsController.cs
+++ Controllers/TweetsController.cs
@@ -20,13 +20,20 @@ namespace TwitterAnalyzer.Controllers
         [HttpPost("classify")]
         public async Task<IActionResult> Classify([FromBody] TweetRequest tweetRequest)
         {
-            var json = JsonConvert.SerializeObject(tweetRequest);
+            var json = JsonConvert.SerializeObject(new { tweets = tweetRequest.Tweets });
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
             var response = await _httpClient.PostAsync("http://localhost:5001/classify", content);
             var classificationResult = await response.Content.ReadAsStringAsync();
 
-            return Ok(classificationResult);
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok(classificationResult);
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, classificationResult);
+            }
         }
     }
 }
